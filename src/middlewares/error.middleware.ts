190dd759f0cef9a5460c51531abb918506aa69ac
Sayyed Sharuk <112304655+sayshark75@users.noreply.github.com>
2025-04-16 22:51:58 +0530

    Update error.middleware.ts

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,54 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import multer from "multer";
 
 // Define the type for the error handler function
 export type ErrorHandlerType = (err: Error, req: Request, res: Response, next: NextFunction) => void;
 
 // Implement the error handler function
 export const errorHandler: ErrorHandlerType = (err, req, res, next) => {
-  // Log the error for debugging purposes
-  console.error(err);
-
-  // Set the default status code for internal server errors
-  let statusCode: number = 500;
-
-  // Check if the error is an instance of a custom error type and adjust the status code accordingly
-  if (err instanceof Error) {
-    switch (err.name) {
-      case "NotFound":
-        statusCode = 404;
-        break;
-      case "Unauthorized":
-        statusCode = 401;
-        break;
-      case "BadRequest":
-        statusCode = 400;
-        break;
-      case "testError":
-        statusCode = 404;
-        break;
-      case "PaymentFailed":
-        statusCode = 404;
-        break;
-      case "PaymentSignatureFailed":
-        statusCode = 404;
-        break;
-      // Add more cases as needed for other custom error types
-    }
+  if (err.message) {
+    return res.status(500).json({
+      error: {
+        message: err.message,
+        status: false,
+      },
+    });
   }
-  if (err instanceof multer.MulterError) {
-    switch (err.message) {
-      case "Unexpected field":
-        err.message = `Unexpected Input field name "${err.field}"`;
-    }
-  }
+
   // Send the error response with the appropriate status code and error details
-  res.status(statusCode).json({
+  res.status(500).json({
     error: {
       message: err.message,
-      name: err.name,
       status: false,
-      stack: process.env.NODE_ENV === "production" ? "🔒" : err.stack, // Stack trace is hidden in production
     },
   });
 };
